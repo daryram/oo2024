547ecdd22acda0acecf22e7360de21a026776bbe
Daria Bayram <146323599+daryram@users.noreply.github.com>
2024-03-14 15:39:16 +0200

    Add files via upload

---
+++ 06_liides/liides/veeb5/arvutus_konst.ts
@@ -0,0 +1,39 @@
+interface CalculatingFunction{
+    calculate(x: number):number;
+    inputUnit():string;
+    outputUnit():string;
+}
+
+class CmToInches implements CalculatingFunction{
+    calculate(inches: number):number{
+        return inches/2.54;
+    }
+    inputUnit(): string {
+        return "cm";
+    }
+    outputUnit(): string{
+        return "in";
+    }
+}
+
+class InchesToCm implements CalculatingFunction{
+    calculate(inches: number):number{
+        return inches*2.54;
+    }
+    inputUnit(): string {
+        return "in";
+    }
+    outputUnit(): string{
+        return "cm";
+    }
+}
+
+class AXplusB implements CalculatingFunction{
+    constructor(protected coeficient:number,
+	  protected inputUnitType:string , protected outputUnitType:string){}
+	calculate(x:number):number{
+	   return this.coeficient*x;
+	}
+	inputUnit():string{return this.inputUnitType;}
+	outputUnit():string{return this.outputUnitType;}
+}
